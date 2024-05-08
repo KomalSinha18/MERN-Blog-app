2dda7682824bf94db45745836bf5e40b5382047f
komal <komalsinha1811@gmail.com>
2024-05-08 14:30:06 +0530

    added call to action to the post page

--- client/src/pages/PostPages.jsx
+++ client/src/pages/PostPages.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import {Link, useParams} from 'react-router-dom'
 import { useEffect,useState } from 'react'
 import { Button, Spinner } from 'flowbite-react'
+import CallToAction from '../components/CallToAction'
 
 export default function PostPages() {
     const {postSlug} = useParams() 
@@ -53,7 +54,10 @@ if(loading) return (
             {post && (post.content.length / 1000).toFixed(0)} mins read
           </span>
         </div>
-        <div dangerouslySetInnerHTML={{__html: post && post.content}} className='p-3 max-w-2xl mx-auto w-full post-content'>1
+        <div  className='p-3 max-w-2xl mx-auto w-full post-content'  dangerouslySetInnerHTML={{__html: post && post.content}}>
+        </div>
+        <div className='max-w-4xl mx-auto w-full'>
+          <CallToAction/>
         </div>
    </main>
   )
