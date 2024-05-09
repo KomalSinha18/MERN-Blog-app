eca15792fc00b421bd778e5db79bbd3a8ed4697f
komal <komalsinha1811@gmail.com>
2024-05-09 20:05:06 +0530

    added edit functionality to the comment  component

--- client/src/components/CommentSection.jsx
+++ client/src/components/CommentSection.jsx
@@ -88,6 +88,14 @@ try {
           console.log(error.message);
         }
       };
+      const handleEdit = async (comment, editedContent) => {
+        setComments(
+          comments.map((c) =>
+            c._id === comment._id ? { ...c, content: editedContent } : c
+          )
+        );
+      };
+
   return (
     <div className='max-w-2xl mx-auto w-full'>
         {
@@ -147,7 +155,7 @@ try {
                 </div>
                     
                 {comments.map((comment,index) => (
-           <Comment key={index} comment={comment} onLike={handleLike} />
+           <Comment key={index} comment={comment} onLike={handleLike} onEdit={handleEdit} />
           ))}
                 </>
 
