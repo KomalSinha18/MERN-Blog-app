049f9e0514e561bda3e142d7cfaa5e9836aed9d0
komal <komalsinha1811@gmail.com>
2024-05-10 14:40:20 +0530

    completed search page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -15,6 +15,7 @@ import CreatePost from './pages/CreatePost'
 import UpdatePost from './pages/UpdatePost'
 import PostPages from './pages/PostPages'
 import ScrollToTop from './components/ScrollToTop'
+import Search from './pages/Search'
 
 export default function App() {
   return (
@@ -26,6 +27,7 @@ export default function App() {
             <Route path='/about' element={<About/>}/>
             <Route path='/sign-in' element={<Signin />}/>
             <Route path='/sign-up' element={<Signup />}/>
+            <Route path='/search' element={<Search />} />
             <Route element={<PrivateRoute/>}>
             <Route path='/dashboard' element={<Dashboard />}/>
             </Route>
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,21 +1,33 @@
 import { Avatar, Button, Dropdown, Navbar, TextInput } from 'flowbite-react'
 import React from 'react'
-import { Link, useLocation, NavLink } from 'react-router-dom'
+import { Link, useLocation, NavLink,useNavigate } from 'react-router-dom'
 import { AiOutlineSearch } from "react-icons/ai";
 import { FaMoon, FaSun} from "react-icons/fa";
 import {useSelector} from "react-redux"
 import { useDispatch } from 'react-redux';
 import { toggleTheme } from '../redux/theme/themeSlice';
 import { signoutSuccess } from '../redux/user/userSlice';
+import { useState , useEffect } from 'react';
 
 
 export default function Header() {
-  
+
+  const navigate = useNavigate();
   // const location = useLocation();
-  const path = useLocation().pathname
+  const location = useLocation()
   const {currentUser} = useSelector((state) => state.user)
   const dispatch = useDispatch()
   const { theme } = useSelector((state) => state.theme);
+  const [searchTerm, setSearchTerm] = useState('')
+console.log(searchTerm);
+  useEffect(() => {
+   const urlParams = new URLSearchParams(location.search)
+   const searchTermFromUrl = urlParams.get('searchTerm')
+   if (searchTermFromUrl) {
+    setSearchTerm(searchTermFromUrl)
+   }
+  }, [location.search])
+  
 
   const handleSignout = async() => {
     try {
@@ -33,6 +45,13 @@ export default function Header() {
       console.log(error.message);
     }
   }
+ const handleSubmit = (e) => {
+    e.preventDefault();
+    const urlParams = new URLSearchParams(location.search);
+    urlParams.set('searchTerm', searchTerm);
+    const searchQuery = urlParams.toString();
+    navigate(`/search?${searchQuery}`);
+  };
   return (
     <Navbar className='border-b-2'>
         <Link to="/" className='self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white'>
@@ -40,12 +59,14 @@ export default function Header() {
                 Komal's</span>
             Blog
         </Link>
-        <form >
+        <form onSubmit={handleSubmit}>
             <TextInput 
                     type='text'
                     placeholder='Search...'
                     rightIcon={AiOutlineSearch}
                     className='hidden lg:inline'
+                    value={searchTerm}
+                    onChange={(e) => setSearchTerm(e.target.value)}
                     />
                     </form>
             <Button className='w-12 h-10 lg:hidden' color='gray' pill>
